48e2ba15df3e5d2ea81281a6e76aaacdfd724c5e
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 03:27:36 -0500

    method isPostalCodeValid

--- src/main/java/org/justinT/Adress.java
+++ src/main/java/org/justinT/Adress.java
@@ -10,7 +10,31 @@ public class Adress {
     private Province province;
     private String postalCode;
 }
+
+/**
+ * checks if a postal code is valid
+ * @param postalCode the postal code to check
+ * @return true if valid, false if not
+ */
 public static boolean isPostalCodeValid(String postalCode) {
+    if (postalCode == null || postalCode.length() != 6) {
+        return false;
+    }
+    for (int i = 0; i < 6; i++) {
+        char ch = postalCode.charAt(i);
+
+        if (i % 2 == 0) {
+            if (!(Character.isLetter(ch))) {
+                return false;
+            }
+        } else {
+            if (!(ch >= '0' && ch <= '9')) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 public Address(int streetNo, String street, String city, Province province, String postalCode) {
 
