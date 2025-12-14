a512f7d6b2e5f5ff781ec0970ddd7c90744c18ee
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 15:16:59 -0500

    method department

--- src/main/java/org/justinT/Department.java
+++ src/main/java/org/justinT/Department.java
@@ -6,9 +6,21 @@ public class Department {
     private String departmentName;
     private static int nextId = 1;
 
-    public Department(String departmentName) {
 
+    /**
+    * Constructor with departmentName
+     * @param departmentName If departmentName is valid, all fields will be set to null.
+     */
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.format("D%02d", nextId++);
+        } else {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
     }
+
 
     public static boolean isDepartmentNameValid(String departmentName) {
 
