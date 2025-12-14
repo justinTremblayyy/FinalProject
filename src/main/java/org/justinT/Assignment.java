102b432888b1c687788edbfe681ed87f027ed92d
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 18:58:21 -0500

    started class 4 (assignment) and imported fields and methods as well as java.util.Random because I saw we will be using randoms. (I am importing fields and methods in advance because it makes it clearer and helps with ADHD)

--- src/main/java/org/justinT/Assignment.java
+++ src/main/java/org/justinT/Assignment.java
@@ -1,4 +1,39 @@
 package org.justinT;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@Getter
+@Setter
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+    private static int nextId = 1;
+
+    public Assignment(String assignmentName, double weight) {
+        this.assignmentName = assignmentName;
+        this.weight = weight;
+        this.scores = new ArrayList<>();
+        this.assignmentId = "A" + String.format("%03d", nextId++);
+    }
+
+    public double calcAssignmentAvg(){
+
+    }
+
+    public void generateRandomScore() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
 }
