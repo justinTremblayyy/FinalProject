361a4f90fff7d7e4859abce04564caeeb7a7daae
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 02:58:40 -0500

    Imported the variables and the methods of class 1 as well as Lombok

---
+++ src/main/java/org/justinT/Adress.java
@@ -0,0 +1,19 @@
+package org.justinT;
+
+public class Adress {
+    public enum Province {
+        QC, ON, NS, NB, MB, BC, PE, SK, AB, NL, NT, YT, NU
+    }
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+}
+public static boolean isPostalCodeValid(String postalCode) {
+
+}
+
+public Address(int streetNo, String street, String city, Province province, String postalCode) {
+
+}
