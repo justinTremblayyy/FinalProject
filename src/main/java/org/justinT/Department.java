406d6a8e38c2d39bbd581e1d61d83d6e796d367f
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 14:46:28 -0500

    started class 2 by importing fields and methods

---
+++ src/main/java/org/justinT/Department.java
@@ -0,0 +1,18 @@
+package org.justinT;
+
+public class Department {
+
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    public Department(String departmentName) {
+
+    }
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+
+    }
+
+
+    }
