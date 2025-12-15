f35a4ebdb0858dae599aa227ba27724a2cbd1150
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-14 21:29:58 -0500

    Started class 5 (Course) and imported the fields and the methods

--- src/main/java/org/justinT/Course.java
+++ src/main/java/org/justinT/Course.java
@@ -1,4 +1,59 @@
 package org.justinT;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+    private static int nextId = 1;
+
+    public Course(){
+
+    }
+
+    public boolean isAssignmentWeightValid() {
+
+    }
+
+    public boolean registerStudent(Student student){
+
+    }
+
+    public int[] calcStudentsAverage(){
+
+    }
+
+    public boolean addAssignment(String assignmentName, double weight, int maxScore) {
+
+    }
+
+    public void generateScores(){
+
+    }
+
+    public void displayScores(){
+
+    }
+
+    public String toSimplifiedString(){
+
+    }
+
+    public String toString(){
+
+    }
+
+
+
 }
