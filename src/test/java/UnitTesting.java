be778e1d55474c9c38a07ff5b80da7680f112442
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2025-12-15 03:21:04 -0500

    testing of address class

--- src/test/java/UnitTesting.java
+++ src/test/java/UnitTesting.java
@@ -11,6 +11,26 @@ public class UnitTesting {
 // Address Class
     @Test
     @DisplayName(
+            "Valid postal code: K2P1L4")
+    void testValidPostalCode() {
+        String postalCode = "K2P1L4";
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("Invalid postal code: AB12CD")
+    void testInvalidPostalCode() {
+        String postalCode = "AB12CD";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
 
 }
